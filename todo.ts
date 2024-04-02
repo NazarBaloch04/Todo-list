6a35e1e422206b3f9d3ed86996585dadbe480809
NazarBaloch04 <153612842+NazarBaloch04@users.noreply.github.com>
2024-04-03 02:51:22 +0500

    Add files via upload

---
+++ todo.ts
@@ -0,0 +1,64 @@
+import inquirer from "inquirer";
+
+
+let todos : string[] = [];
+
+async function createTodo(todos:string[]){
+    do{let ans = await inquirer.prompt({
+        type: "list",
+        message: "select an operation",
+        name: "select",
+        choices: ["Add","Update","View","Delete"],
+    })
+      if (ans.select == "Add"){
+        let addTodo = await inquirer.prompt({
+            type: "input",
+            message: "Add items in the list",
+            name: "todo",
+        });
+        todos.push(addTodo.todo);
+        todos.forEach(todo => console.log(todo));
+        
+      
+      }
+
+         if (ans.select == "Update"){
+            let updateTodo = await inquirer.prompt({
+                type: "list",
+                message: "Update items in the list",
+                name: "todo",
+                choices: todos.map(item => item)
+            });
+
+            let addTodo = await inquirer.prompt({
+                type: "input",
+                message: "Add items in the list",
+                name: "todo",
+            });
+            let newTodo = todos.filter(val =>val !== updateTodo.todo);
+            todos = [...newTodo,addTodo.todo];
+            console.log(todos);
+            
+            }
+
+            if (ans.select == "View"){
+                console.log("*** TO DO List ***");
+                console.log(todos);
+                console.log("*");
+                 }
+
+      if (ans.select == "Delete"){
+        let deleteTodo = await inquirer.prompt({
+            type: "list",
+            message: "Update items in the list",
+            name: "todo",
+            choices: todos.map(item => item)
+        });
+        let newTodo = todos.filter(val =>val !== deleteTodo.todo);
+            todos = [...newTodo];
+            console.log(todos);
+      }} while(true)    
+}
+createTodo(todos);
+
+
